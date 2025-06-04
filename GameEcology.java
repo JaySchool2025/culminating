86faaac928e03766c5e940060323e57134308e97
AriSug <ari.sugarman@student.tdsb.on.ca>
2025-06-04 15:06:39 -0400

    Update 06/04 P4

---
+++ GameEcology.java
@@ -0,0 +1,24 @@
+public class GameEcology{
+   
+   
+   public void TempCalc(){
+      
+   }
+   
+   public void PlantDeathMech(){
+      
+   }
+   
+   public void PlantRegrowMech(){
+      
+   }
+   
+   public void TimeSincePDRMechInitiationCalc(){
+      
+   }
+   
+   public void OverheatEndingTracker(){
+      
+   }
+   
+}
---
+++ GameMain.java
@@ -0,0 +1,24 @@
+public class GameMain{
+   
+   
+   public void FirstMessage(){
+      
+   }
+   
+   public void OverviewTimePassage(){
+      
+   }
+   
+   public void BalanceSheet(){
+      
+   }
+   
+   public void CO2Emission(){
+      
+   }
+   
+   public void PlantMerchant(){
+      
+   }
+   
+}
