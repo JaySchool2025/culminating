d446e1553141eafda9a46a1c437c06e5d8303c13
AriSug <ari.sugarman@student.tdsb.on.ca>
2025-06-09 13:56:00 -0400

    Update 06/09 P3

--- GameMain.java
+++ GameMain.java
@@ -3,9 +3,7 @@ import java.util.*;
 
 public class GameMain{
    
-   GameMain(){
-      Scanner s = new Scanner(System.in);
-   }
+   Scanner s = new Scanner(System.in);
    
    public void FirstMessage(){
       
@@ -42,20 +40,100 @@ public class GameMain{
       System.out.println("Total Energy Producing Plants: "/* + total plant count minus PS plant count*/);
       System.out.println("Photosynthesizing Plants: "/* + PS plant count*/);
       
-      System.out.printf("/nWould you like to proceed to the next year, proceed by 5 years, or go to a specific menu?/n1 - Proceed 1 year./n2 - Proceed 5 years./n3 - Choose a specific menu.");
+      //if(/*balance < 0*/){
       
+        // System.out.printf("/n/nBalance is negative. Exact value: /d. Please be wary of this."/* + balance*/);
+      
+      //}
+      //if(/*co2 emissions >= 1*/){
+         
+      //   System.out.printf("/n/nCO2 Emissions are above 1. Exact value: /.2f. Please be wary of this."/* + co2 emissions*/);
+         
+      //}
+      //if(/*temperature >= 15.2*/){
+      
+      //   System.out.printf("/n/nTemperature is above 15.2°C. Exact value: /.1f. Please be wary of this."/* + temperature*/);
+      
+      //}
+      
+      System.out.printf("/nWould you like to proceed to the next year, proceed by 5 years, or go to a specific menu?/n1 - Proceed 1 year./n2 - Proceed 5 years./n3 - Choose a specific menu.");
+      int choice = s.nextInt();
+      
+      /*if(choice == 1){
+         
+         //proceed 1 year, call methods to proceed
+         OverviewTimePassage();
+         
+      }
+      else if(choice == 2){
+         
+         //proceed 5 years, call methods to proceed 5 times
+         OverviewTimePassage();
+         
+      }
+      else */if(choice == 3){
+         
+         while(1 == 1){
+         System.out.printf("/nPlease choose the menu that you would like to go to./n1 - Detailed Balance Sheet of Energy Income and Plant Maintenance./n2 - Detailed Sheet for CO2 Emissions and Absorption./n3 - Menu for buying & selling Plants (All Types)./n4 - Return to Overview.");
+         int menuchoice = s.nextInt();
+         
+         if(menuchoice == 1){
+            
+            break;
+            BalanceSheet();
+            
+         }
+         else if(menuchoice == 2){
+            
+            break;
+            CO2Emission();
+            
+         }
+         else if(menuchoice == 3){
+            
+            break;
+            PLantMerchant();
+            
+         }
+         else if(menuchoice == 4){
+            
+            break;
+            OverviewTimePassage();
+            
+         }
+         else{
+            
+            System.out.println("Please enter a value between 1 and 4. The input you provided is invalid. The question will be asked again.");
+         
+         }
+         }
+      }
+      else{
+         
+         System.out.println("Please enter a value between 1 and 3. The input you provided is invalid. The question will be asked again.");
+         OverviewTimePassage();
+         
+      }
    }
    
    public void BalanceSheet(){
-      
+      System.out.println("Placeholder Balance Sheet");
+      OverviewTimePassage();
    }
    
    public void CO2Emission(){
-      
+      System.out.println("Placeholder CO2 Emissions");
+      OverviewTimePassage();
    }
    
    public void PlantMerchant(){
-      
+      System.out.println("Placeholder Plant Merchant");
+      OverviewTimePassage();
+   }
+   
+   public static void main (String[] args){
+      GameMain g = new GameMain();
+      g.FirstMessage();
    }
    
 }
