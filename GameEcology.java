c8951e5fd87281e8f26cc61a3d930c9059dabd07
AriSug <ari.sugarman@student.tdsb.on.ca>
2025-06-11 13:54:54 -0400

    Update 06/11 P3

--- GameEcology.java
+++ GameEcology.java
@@ -1,24 +1,69 @@
 public class GameEcology{
+   int psTotal = psCount;
+   double temp;
+   double prevtemp;
+   double co2;
+   int TimeSincePDRMech;
    
    
    public void TempCalc(){
       
+      if(co2 > 0){
+         temp = prevtemp + (co2 / 100);
+      }
+      else if(co2 < 0){
+         temp = prevtemp + (co2 / -1000);
+      }
+      else{
+         temp = prevtemp;
+      }
    }
    
-   public void PlantDeathMech(){
-      
+   public void PlantDeathRegrowthMech(){
+      if(TimeSincePDRMech >= 2 && temp >= 16.7 && temp < 17.7){
+         psCount = psCount - 250;
+         if(psCount < 0){
+          psCount = 0;
+         }
+      }
+      else if(TimeSincePDRMech >= 2 && temp >= 17.7){
+         psCount = psCount - 500;
+         if(psCount < 0){
+          psCount = 0;
+         }
+      }
+      else if(TimeSincePDRMech >= 2 && temp <= 16.2){
+         psCount = psCount + 50;
+         if(psCount > psTotal){
+          psCount = psTotal;
+         }
+      }
    }
    
-   public void PlantRegrowMech(){
-      
-   }
    
    public void TimeSincePDRMechInitiationCalc(){
+      if(temp <= 16.2 && prevtemp <= 16.2 || temp >= 16.7 && prevtemp >= 16.7){
+         TimeSincePDRMech = TimeSincePDRMech + 1;
+      }
+      else{
+         TimeSincePDRMech = 0;
+      }
       
    }
    
    public void OverheatEndingTracker(){
+      boolean HeatEnd = false;
       
+      if(temp <= TempEnding){
+      
+         HeatEnd = true;
+         System.out.println("The player has reached a global temperature of 19.7°C. The simulation will now end as the player has overheated the world by enough to cause severe damage. Good luck, please try again.");
+         System.exit(0);
+         
+      }
+      else{
+         HeatEnd = false;
+      }
    }
    
 }
--- GameEconomy.java
+++ GameEconomy.java
@@ -1,23 +1,70 @@
 public class GameEconomy{
    
+   int cCount
+   int ngCount
+   int nCount
+   int gCount
+   int psCount
    
-   public IncomeCalc(){
-      
+   //c = coal, ng = natural gas, n = nuclear, g = green, ps = photosynthesising, En = energy, Ma = maintenance, Pen = penalty
+   
+   public void IncomeCalc(){
+      double income = (((cCount * cEn) + (ngCount * ngEn) + (nCount * nEn) + (gCount * gEn)) * 100);
+      // 100 = 100M
    }
    
    public void MaintenanceCalc(){
-      
+      double maintenance = ((cCount * cMa) + (ngCount * ngMa) + (nCount * nMa) + (gCount * gMa));
    }
    
    public void BalanceAndUpkeep(){
-      
+      double balance = (income * EnPen) - (maintenance * MaPen);
    }
    
    public void PenaltyTracker(){
+      double EnPen;
+      double MaPen;
+      
+      if(temp >= 17.2 && temp < 18.2){
+         
+        EnPen = FirstEnergy;
+        MaPen = 1;
+         
+      }
+      else if(temp >= 18.2 && temp < 18.7){
+         
+         EnPen = SecondEnergy;
+         MaPen = FirstCost;
+         
+      }
+      else if (temp >= 18.7){
+         
+         EnPen = ThirdEnergy;
+         MaPen = SecondCost;
+         
+      }
+      else{
+         
+         EnPen = 1;
+         MaPen = 1;
+      
+      }
       
    }
    
    public void DebtEndingTracker(){
+      boolean DebtEnd = false;
+      
+      if(playerbalance <= DebtEnding){
+      
+         DebtEnd = true;
+         System.out.println("The player has reached a balance of $-10,000,000. The simulation will now end as the player has gone sufficiently into debt. Good luck, please try again.");
+         System.exit(0);
+         
+      }
+      else{
+         DebtEnd = false;
+      }
       
    }
    
